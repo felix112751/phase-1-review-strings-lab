b6b2f73c198cce7e949c4dd7f09f806b11a41267
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-06-24 21:29:50 +0300

    review of strings

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Felix Goyeh';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome,  ${firstInitial}!`
+
+ 
+
+
+
